afe193876ee8ba1edb01d5a5110ac364b8e4da51
conchabb <cpbbbarroso@gmail.com>
2020-10-10 13:45:41 +0100

    with articulation points qb

--- biconnected.c
+++ biconnected.c
@@ -5,6 +5,7 @@ void isBiconnected(Graph* graph){
 	int visited[N];
 	int d[N];
 	int l[N];
+	int pred[N];
 	int time=0;
 	int i;
 
@@ -12,19 +13,24 @@ void isBiconnected(Graph* graph){
 		visited[i]=0;
 		d[i]=0;
 		l[i]=0;
+		pred[i]=-1;
 	}
 
 	//start algorithm in node 1
 	//DFS(graph, 0, visited);
 
-	/*for(i=0;i<N;i++){
-		printf("%d ", visited[i]);
-	}*/
+	//root is node 1
+	artPoints(graph, 0, visited, d, l, pred, time);
+
+	for(i=0;i<N;i++){
+		printf("%d ", d[i]);
+		printf("%d\n", l[i]);
+	}
 
 	return;
 }
 
-void artPoints(Graph* graph, int n, int visited[], int d[], int l[], int time){
+void artPoints(Graph* graph, int n, int visited[], int d[], int l[], int pred[], int time){
 
 	Node* aux;
 	aux=graph->nodes[n];
@@ -34,18 +40,43 @@ void artPoints(Graph* graph, int n, int visited[], int d[], int l[], int time){
 	d[n]=time;
 	l[n]=d[n];
 	
-	printf("%d\n", n);
+	printf("%d, ", n);
+	printf("%d\n", time);
 	
-	if(graph->nodes[n]!=NULL){
+	if(aux->next!=NULL){
 		
-		if(visited[aux->head]==0)
-			artPoints(graph, aux->head, visited, d, l);
-
+		if(visited[aux->head]==0){
+			pred[aux->head]=n;
+			artPoints(graph, aux->head, visited, d, l, pred, time);
+			if(l[aux->head]>=d[n]){
+				printf("%d is an articulation point\n",n);
+			}
+			l[n]=MIN(l[n], l[aux->head]);
+		}
+		//back edge
+		else if (aux->head!=pred[n]){
+			l[n]=MIN(l[n], d[aux->head]);
+		}
+		
         while(aux->next!=NULL){
             aux=aux->next;
 
-			if(visited[aux->head]==0)
-				artPoints(graph, aux->head, visited, d, l);
+			if(visited[aux->head]==0){
+				//if is root and second child
+				if(n==0){
+					printf("%d is an articulation point\n",n);
+				}
+				pred[aux->head]=n;
+				artPoints(graph, aux->head, visited, d, l, pred, time);
+				if(l[aux->head]>=d[n]){
+					printf("%d is an articulation point\n",n);
+				}
+				l[n]=MIN(l[n], l[aux->head]);
+			}
+			//back edge
+			else if (aux->head!=pred[n]){
+				l[n]=MIN(l[n], d[aux->head]);
+			}	
         }
     }
 }
--- biconnected.h
+++ biconnected.h
@@ -4,8 +4,9 @@
 #include "graph.h"
 
 #define N 11
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 
 void isBiconnected(Graph* graph);
-void artPoints(Graph* graph, int n, int visited[], int d[], int l[], int time);
+void artPoints(Graph* graph, int n, int visited[], int d[], int l[], int pred[], int time);
 void DFS(Graph* graph, int n, int visited[]);
