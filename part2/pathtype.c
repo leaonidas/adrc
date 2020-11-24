f3b13ef226b2d71f735274e7160b03457b3add11
leaonidas <mtmleao@gmail.com>
2020-11-24 12:04:06 +0000

    dijkstra com commercial path

--- part2/pathtype.c
+++ part2/pathtype.c
@@ -8,6 +8,7 @@ void pathType(Graph* graph, int src, int dest){
     double inf = 70000;
     int dist[graph->n];
     int prev[graph->n];
+    int typePrev[graph->n];
     bool visited[graph->n];
     Node* aux;
 
@@ -15,35 +16,58 @@ void pathType(Graph* graph, int src, int dest){
         dist[i] = inf; prev[i] = -1; visited[i] = false;
     }
     dist[src-1] = 0;
+    typePrev[src-1] = 0;
     
     for(i = 0; i < graph->n; i++){
         visit = nextVisit(dist, visited, graph->n);
+        if(visit == -1){
+            printf("No commercial path found between nodes!\n");
+            return;
+        }
         visited[visit] = true;
         printf("Nó em que está: %d\n", visit);
         aux = graph->nodes[visit];
         while(aux != NULL){
-            if(!visited[aux->head] && (dist[visit] + 1) < dist[aux->head]){
+            if(!visited[aux->head] && (dist[visit] + 1) < dist[aux->head] && verifyComm(typePrev[visit], aux->type)){
                 dist[aux->head] = dist[visit] + 1;
                 prev[aux->head] = visit;
+                typePrev[aux->head] = aux->type;
                 printf("Nó vizinho visitado: %d   Distância até ao nó: %d\n", aux->head, dist[aux->head]);
+                if(aux->head == dest-1){
+                    printSolution(dist, prev, src, dest);
+                    return;
+                }
             }
             aux = aux->next;
             printf("\n\n");
         }
     }
-    printSolution(dist, prev, src, dest);
-
 }
 
 int nextVisit(int* dist, bool* visited, int size){
-    int min = 70000, next = 0, i = 0;
+    int min = 70000, next = -1, i = 0;
     for(i = 0; i < size; i++){
-        if(dist[i] <= min && visited[i] == false){
+        if(dist[i] < min && visited[i] == false){
             next=i; min = dist[i];
         }
     }
     return next;
 }
+
+bool verifyComm(int prevConn, int nextConn){
+
+    if(prevConn == 0) return true;
+
+    if(nextConn == 1) return true;
+
+    else if(nextConn == 2 && prevConn == 3) return true;
+
+    else if(nextConn == 3 && prevConn == 3) return true;
+
+    return false;
+
+}
+
 
 void printSolution(int* dist, int* prev, int src, int dest){
     int aux = dest-1;
--- part2/pathtype.h
+++ part2/pathtype.h
@@ -4,4 +4,5 @@
 
 void pathType(Graph* graph, int src, int dest);
 int nextVisit(int* dist, bool* visited, int size);
+bool verifyComm(int prevConn, int nextConn);
 void printSolution(int* dist, int* prev, int src, int dest);
